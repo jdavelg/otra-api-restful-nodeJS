ca89e4d3a6180e99ea24cdb4578ec78ac754580d
jonathan lopez <jdavelg@gmail.com>
2020-07-18 14:46:17 -0600

    third commit

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ var port= process.env.PORT || 3999
 mongoose.set('useFindAndModify', false)
 mongoose.Promise= global.Promise;
 { useUnifiedTopology: true }
-mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser:true })
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/api_rest_node', { useNewUrlParser:true })
 .then(()=>{
     console.log("la conexion a la BD de mongo de ha realizado correctamente");
 //crear el servidor
-
-app.listen(port, ()=>{
-    console.log("el servidor is running good");
+app.listen(process.env.PORT || 8080
     
-})
+    )
 
 
     
